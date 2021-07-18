e8a3a72d678f2ba32b7b50e3c35cd5f71bfba160
bermanomri <bermanomri@gmail.com>
2021-07-18 16:31:59 +0300

    complete visualizations for final report

--- main.py
+++ main.py
@@ -5,27 +5,30 @@ import pandas
 import pandas as pd
 from sklearn.datasets import make_spd_matrix
 import scipy.stats as stats
+
 font = {'size': 16}
 matplotlib.rc('font', **font)
 matplotlib.rc('figure', **{"figsize": (19.2, 9.77), "autolayout": True})
 #
 import seaborn as sns
-from utils import confidence_ellipse
+from utils import confidence_ellipse, plotData, plotDist, to_numpy, res_path, plotCaviProgress, genResDir, genGif
+from pathlib import Path
 
-def getMogData(K:int=5, sigma:float=1, nDims:int=2 ,nSamples:int=100, debug:bool=True):
+def getMogData(K: int = 5, sigma: float = 1, nDims: int = 2, nSamples: int = 100, debug: bool = True):
     """Generates a MOG model and draws nSamples from it"""
-    mu_k = np.random.multivariate_normal(mean=np.zeros(shape=nDims), cov=sigma**2 * np.identity(nDims), size=K)
+    mu_k = np.random.multivariate_normal(mean=np.zeros(shape=nDims), cov=sigma ** 2 * np.identity(nDims), size=K)
     c = np.random.choice(range(K), size=nSamples, p=np.ones(shape=K) / K)
     mu_c = mu_k[c]
     cov_k = np.zeros((K, nDims, nDims))
     for k in range(K):
-    #     randSquaredMat = np.random.normal(size=(nDims, nDims))
-    #     cov_k[k] = randSquaredMat.T @ randSquaredMat
-    #     # cov_k[k] = make_spd_matrix(n_dim=nDims)
+        #     randSquaredMat = np.random.normal(size=(nDims, nDims))
+        #     cov_k[k] = randSquaredMat.T @ randSquaredMat
+        #     # cov_k[k] = make_spd_matrix(n_dim=nDims)
         cov_k[k] = np.identity(nDims)
 
     x_mu_c = lambda i: np.random.multivariate_normal(mean=mu_c[i].squeeze(axis=0), cov=cov_k[c[i]].squeeze(axis=0))
-    x = np.apply_along_axis(x_mu_c, axis=1, arr=np.expand_dims(np.arange(nSamples), axis=1))# np.random.multivariate_normal(mu_c, cov=np.identity(nDims))
+    x = np.apply_along_axis(x_mu_c, axis=1, arr=np.expand_dims(np.arange(nSamples),
+                                                               axis=1))  # np.random.multivariate_normal(mu_c, cov=np.identity(nDims))
     x_df = pd.DataFrame(index=np.arange(nSamples))
     x_df["x"] = x.tolist()
     x_df["c"] = c
@@ -42,17 +45,13 @@ def getMogData(K:int=5, sigma:float=1, nDims:int=2 ,nSamples:int=100, debug:bool
         # ax[1].axis("equal")
         for k in range(K):
             k_data = x_df.loc[x_df.c == k]
+            plotData(k_data, ax[1], f"k={k}")
+
             mean = np.array(k_data.mu.iloc[0])
             cov = np.array(k_data.loc[:, "cov"].iloc[0])
-            # plotGauusian(mean, cov, f"k={k}", ax[1])
             getCoord = lambda data, i: np.array([lst[i] for lst in data])
-            ax[1].plot(getCoord(k_data.x, 0), getCoord(k_data.x, 1), 'o', label=f"k={k}")
             confidence_ellipse(getCoord(k_data.x, 0), getCoord(k_data.x, 1), ax=ax[1],
                                facecolor=ax[1].lines[-1].get_color(), alpha=0.5)
-            # x_span = np.linspace(mean - 3*var, mean + 3*var, 100)
-            # ax[1].plot(x_span, stats.norm.pdf(x_span, mean, var), label=f"k={k}")
-            # ax[1].scatter(k_data.x, stats.norm.pdf(k_data.x, mean, var))
-
 
         # ax[1].axes.set_aspect('equal')
         ax[1].legend()
@@ -61,28 +60,12 @@ def getMogData(K:int=5, sigma:float=1, nDims:int=2 ,nSamples:int=100, debug:bool
         ax[1].set_ylabel("$p(x|c, \mu)$")
         # ax[1].set_ylim((0, 0.5))
         ax[1].grid()
+        fig.savefig(Path(res_path, "dataDist.png"))
 
     return x_df
 
-def plotGauusian(mean, cov, label, ax, *args):
-    dist = stats.multivariate_normal(mean=mean, cov=cov)
-    std = np.sqrt(cov)
-    plotSpan = lambda i: np.linspace(mean[i] - 3 * std.diagonal()[i], mean[i] + 3 * std.diagonal()[i], 100)
-    if len(mean) > 1:
-        diag = std.diagonal()
-        x, y = plotSpan(0), plotSpan(1)
-        confidence_ellipse(x, y, ax=ax)
-        ax.pcolormesh(x.flatten().T, y.flatten().T, dist.pdf(plot_span.T), shading='auto', cmap=plt.cm.BuGn_r, label=label, *args)
-    else:
-        plot_span = plotSpan(std)
-        ax.plot(plot_span, dist.pdf(plot_span), label=label, *args)
 
-
-def to_numpy(seriesOfLists):
-    listOfLists = [x for x in seriesOfLists]
-    return np.array(listOfLists)
-
-def CAVI(data:pd.DataFrame, sigma:float, debug=True):
+def CAVI(data: pd.DataFrame, sigma: float, debug=True, path=res_path):
     """Coordinate Ascent Variational Inference for GMM"""
 
     n = len(data)
@@ -94,6 +77,9 @@ def CAVI(data:pd.DataFrame, sigma:float, debug=True):
     s2 = np.ones(shape=(dims, K))
     phi = np.ones(shape=(n, K)) / K
 
+    if debug:
+        plotCaviProgress(data, m, s2, path=path)
+
     x = to_numpy(data.x)  # set data as (n,1) ndarray
 
     # implementation:
@@ -101,13 +87,14 @@ def CAVI(data:pd.DataFrame, sigma:float, debug=True):
     conv_tol = 1e-5
     elbo = []
     iter = 0
-    while not is_conv:
+    while not is_conv and iter < 100:
+
         # update variational assignments:
-        phi = np.exp(x @ m - 0.5 * (s2 + m**2).sum(axis=0))
+        phi = np.exp(x @ m - 0.5 * (s2 + m ** 2).sum(axis=0))
         phi /= phi.sum(axis=1, keepdims=True)
 
         # update variational densities:
-        den = 1/sigma**2 + phi.sum(axis=0, keepdims=True)
+        den = 1 / sigma ** 2 + phi.sum(axis=0, keepdims=True)
         m = (x.T @ phi) / den
         s2 = 1 / den
 
@@ -115,39 +102,25 @@ def CAVI(data:pd.DataFrame, sigma:float, debug=True):
         if len(elbo) > 2 and np.abs(elbo[-2] - elbo[-1]) < conv_tol:
             is_conv = True
 
-        if debug:
-            fig1, ax1 = plt.subplots()
-            for k in range(K):
-                k_data = data.loc[data.c == k]
-                getCoord = lambda data, i: np.array([lst[i] for lst in data])
-                ax1.plot(getCoord(k_data.x, 0), getCoord(k_data.x, 1), 'o', label=f"k={k}")
-                mean = np.random.multivariate_normal(mean=m[:, k].T, cov=np.identity(dims) * s2[:, k])
-                cov = np.identity(dims)
-                samples_hat = np.random.multivariate_normal(mean=mean, cov=cov, size=int(1e5))
-                dist_label = "Variational Distributions" if k == 0 else None
-                confidence_ellipse(samples_hat[:, 0], samples_hat[:, 1], ax=ax1,
-                                   facecolor="k", alpha=0.5, label=dist_label)
-
-            ax1.set_xlabel("$x_1$")
-            ax1.set_xlabel("$x_2$")
-            ax1.set_title(f"q(z) (Iteration #{iter})")
-            ax1.legend()
-            fig1.savefig(f"Figs/CAVI_{iter}.png")
-            plt.close(fig1)
-
         iter += 1
 
+        ## Plot the current state
+        if debug:
+            plotCaviProgress(data, m, s2, iter, path=path)
+
+
     if debug:
-        fig0, ax0= plt.subplots()
+
+        fig0, ax0 = plt.subplots()
         ax0.plot(elbo)
         ax0.set_xlabel("Iteration")
         ax0.set_ylabel("ELBO(q)")
         ax0.set_title("CAVI Convergence Plot")
         ax0.grid()
-        fig0.savefig("Figs/elbo.png")
+        fig0.savefig(Path(path, "elbo.png"))
         plt.close(fig0)
-    return phi, m, s2, elbo
 
+    return phi, m, s2, elbo
 
 
 def calcElbo(x: np.ndarray, sigma: float, m_hat: np.ndarray, s2_hat: np.ndarray, phi_hat: np.ndarray):
@@ -161,24 +134,32 @@ def calcElbo(x: np.ndarray, sigma: float, m_hat: np.ndarray, s2_hat: np.ndarray,
             phi_hat - (n,K) variational assignments matrix
     """
     elbo_terms = np.zeros(4)  # prepare a vector for the different elbo terms
-    elbo_terms[0] = -1/(2*sigma**2) * (s2_hat + (m_hat**2).sum(axis=0)).sum()
-    elbo_terms[1] = (phi_hat * (x @ m_hat - 0.5*(s2_hat + (m_hat**2)).sum(axis=0))).sum()
+    elbo_terms[0] = -1 / (2 * sigma ** 2) * (s2_hat + (m_hat ** 2).sum(axis=0)).sum()
+    elbo_terms[1] = (phi_hat * (x @ m_hat - 0.5 * (s2_hat + (m_hat ** 2)).sum(axis=0))).sum()
     elbo_terms[2] = -(phi_hat * np.log(phi_hat)).sum()
-    elbo_terms[3] = -0.5*np.log(2 * s2_hat).sum()
+    elbo_terms[3] = -0.5 * np.log(2 * s2_hat).sum()
 
     return elbo_terms.sum()
 
+
 if __name__ == "__main__":
-    sigma = 5
+    sigma = 3
     K = 5
     data = getMogData(K=K, sigma=sigma, nSamples=int(1e3))
 
     # inspect ELBOs sensitivity to initialization:
-    phi, m, s2, elbo = CAVI(data, 1, debug=True)
-    # fig, ax = plt.subplots()
-    # for i in range(10):
-    #     phi, m, s2, elbo = CAVI(data, 1, debug=False)
-    #     ax.plot(elbo, 'b')
-    # ax.set_xlabel("Iterations")
-    # ax.set_ylabel("Elbo(q)")
-    # ax.set_title("ELBO sensitivity to Different Initializations")
+    # phi, m, s2, elbo = CAVI(data, 1, debug=True)
+    fig, ax = plt.subplots()
+    for i in range(10):
+        exp_path = Path(res_path, str(i))
+        exp_path.mkdir()
+        phi, m, s2, elbo = CAVI(data, 1, debug=True, path=exp_path)
+        ax.plot(elbo, 'b')
+        genGif(exp_path)
+    ax.set_xlabel("Iterations")
+    ax.set_ylabel("Elbo(q)")
+    ax.set_title("ELBO Sensitivity to Initialization")
+    ax.grid()
+    fig.savefig(Path(res_path, "elbo.png"))
+    plt.close(fig)
+
--- utils.py
+++ utils.py
@@ -2,6 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
+from pathlib import Path
+
+figPath = Path("Figs")
+import datetime
+
+
+def genResDir():
+    now = datetime.datetime.now()
+    res_dir = "_".join([str(x) for x in [now.day, now.month, "", now.hour, now.minute, now.second, now.microsecond]])
+    path = Path(figPath, res_dir)
+    path.mkdir()
+    return path
+
+
+res_path = genResDir()
+
 
 def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
     """
@@ -29,7 +45,7 @@ def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
         raise ValueError("x and y must be the same size")
 
     cov = np.cov(x, y)
-    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    pearson = cov[0, 1] / np.sqrt(cov[0, 0] * cov[1, 1])
     # Using a special case to obtain the eigenvalues of this
     # two-dimensionl dataset.
     ell_radius_x = np.sqrt(1 + pearson)
@@ -54,3 +70,79 @@ def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
 
     ellipse.set_transform(transf + ax.transData)
     return ax.add_patch(ellipse)
+
+
+def plotData(data, ax, label):
+    getCoord = lambda data_in, i: np.array([lst[i] for lst in data_in])
+    ax.plot(getCoord(data.x, 0), getCoord(data.x, 1), 'o', label=label, alpha=0.5)
+
+
+def plotPostPred(mu, sigma2, ax, label):
+    """Plot the distribution of the Posterior Predictive"""
+    dims = len(mu)
+    mean = np.random.multivariate_normal(mean=mu.T, cov=np.identity(dims) * sigma2)
+    cov = np.identity(dims)
+    samples_hat = np.random.multivariate_normal(mean=mean, cov=cov, size=int(1e5))
+    confidence_ellipse(samples_hat[:, 0], samples_hat[:, 1], ax=ax, facecolor="k", alpha=0.25, label=label)
+
+
+def plotDist(mu, sigma2, ax, label, alpha=0.5):
+    """Plot the distribution of the Gaussian detremined by mean mu_k and covariance sigma2"""
+    dims = len(mu)
+    samples_hat = np.random.multivariate_normal(mean=mu.T, cov=np.identity(dims) * sigma2, size=int(1e5))
+    confidence_ellipse(samples_hat[:, 0], samples_hat[:, 1], ax=ax, facecolor="k", alpha=alpha, label=label)
+
+
+def plotCaviProgress(data, m, s2, iter=None, path=res_path):
+    K = len(data.c.unique())
+    fig, ax = plt.subplots(2, 1)
+    for k in range(K):
+        k_data = data.loc[data.c == k]
+
+        # Plot Gaussian Variational Factor
+        plotData(k_data, ax[0], f"k={k}")
+        dist_label = "$\mathcal{N}(\mu_k;m_k, s^2_k)$" if k == 0 else None
+        plotDist(m[:, k], s2[:, k], ax[0], dist_label, alpha=0.7)
+
+        # Plot Posterior Predictive:
+        plotData(k_data, ax[1], f"k={k}")
+        dist_label = "$\mathcal{N}(\mu_k, 1)$" if k == 0 else None
+        plotDist(m[:, k], 1, ax[1], dist_label, alpha=0.25)
+
+    data_np = to_numpy(data.x)
+    for axis in ax:
+        axis.set_xlabel("$x_1$")
+        axis.set_ylabel("$x_2$")
+        axis.set_xlim([data_np[:, 0].min() * 1.1, data_np[:, 0].max() * 1.1])
+        axis.set_ylim([data_np[:, 1].min() * 1.1, data_np[:, 1].max() * 1.1])
+        axis.legend()
+
+    ax[0].set_title("Variational Factors $q(\mu_k)$")
+    ax[1].set_title(r"Posterior Predictive Approximation $p(x_{new}|x) \approx \frac{1}{K}\sum_1^kp(x_{new};m_k)$")
+
+    if iter is None:
+        suffix = "Initial State"
+    else:
+        suffix = f"Iteration #{iter}"
+
+    fig.suptitle(f"CAVI Progress ({suffix})")
+    fig.savefig(Path(path, f"CAVI_{suffix}.png"))
+    plt.close(fig)
+
+
+def to_numpy(seriesOfLists):
+    listOfLists = [x for x in seriesOfLists]
+    return np.array(listOfLists)
+
+def genGif(path, dur=150):
+    from PIL import Image
+    flist = [x for x in path.iterdir() if x.match("*CAVI*")]
+    flist.sort(key=lambda item: item.lstat().st_mtime)
+    pil_list = [Image.open(img) for img in flist]
+    for i in range(500 // dur):
+        pil_list.insert(0, pil_list[0])
+    pil_list[0].save(Path(path, "ilustration.gif"), save_all=True, append_images=pil_list[1:], loop=0, duration=dur)
+
+
+if __name__=="__main__":
+    genGif(Path("/home/omri/PycharmProjects/SignalProcessing2DataScience/FinalProject/Figs/sameData2/success2"))
